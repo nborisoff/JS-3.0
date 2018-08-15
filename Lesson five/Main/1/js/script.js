205bb40d9f0eb775e75b9cef3a5d7f66e1128796
nborisov256 <41783639+nborisov256@users.noreply.github.com>
2018-08-15 22:55:59 +0300

    Обязательное задание. Часть первая.

---
+++ Lesson five/Main/1/js/script.js
@@ -0,0 +1,23 @@
+let menuItem = document.getElementsByClassName('menu-item'),
+    ul = document.getElementsByTagName('ul')[0],
+	body = document.querySelector('body'),
+	title = document.getElementById('title'),
+	adv = document.getElementsByClassName('adv'),
+	question = document.getElementById('prompt');
+	
+
+menuItem[1].textContent = 'Второй пункт';
+menuItem[2].textContent = 'Третий пункт';
+
+let li = document.createElement('li');
+li.classList.add("menu-item");
+li.textContent = 'Пятый пункт';
+ul.appendChild(li);
+
+body.style.background = 'url(img/apple_true.jpg) center no-repeat';
+
+title.textContent = 'Мы продаем только подлинную технику Apple';
+
+adv[0].remove();
+
+question.textContent = prompt('Как вы относитесь к технике Apple?','');;

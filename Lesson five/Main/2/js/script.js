d6d258b6bc4936ca310e68e2da7e777f0e8d0bb4
nborisov256 <41783639+nborisov256@users.noreply.github.com>
2018-08-16 14:25:15 +0300

    Обязательное задание. Часть вторая.

--- Lesson five/Main/2/js/script.js
+++ Lesson five/Main/2/js/script.js
@@ -1,4 +1,5 @@
 let openButton = document.getElementById('open-btn'),
+    
 	nameValue = document.getElementsByClassName('name-value'),
 	budgetValue = document.getElementsByClassName('budget-value'),
 	goodsValue = document.getElementsByClassName('goods-value'),
@@ -7,11 +8,13 @@ let openButton = document.getElementById('open-btn'),
 	discountValue = document.getElementsByClassName('discount-value'),
 	isopenValue = document.getElementsByClassName('isopen-value'),
 	chooseGoods = document.getElementsByClassName('goods-item'),
-	allButtons = document.getElementsByClassName('main-functions'),
+
 	buttonOne = document.getElementsByTagName('button')[1],
 	buttonTwo = document.getElementsByTagName('button')[2],
 	buttonThree = document.getElementsByTagName('button')[3],
+    
 	chooseItem = document.querySelector('.main-functions .choose-item'),
 	time = document.querySelector('.main-functions .time-value'),
 	countBudget = document.querySelector('.main-functions .count-budget-value'),
+    
 	hireEmployers = document.querySelectorAll('.main-functions .hire-employers-item'); 

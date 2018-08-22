e09a6b3114ce3a237ac50d0bb2b9fa09175d88b4
nborisov256 <41783639+nborisov256@users.noreply.github.com>
2018-08-22 21:30:28 +0300

    Обязательное задание

--- Lesson nine/Main/this/js/script.js
+++ Lesson nine/Main/this/js/script.js
@@ -1,12 +1,8 @@
 let age = document.getElementById('age');
  
-let user = {
-	value: age.value
-}
-
 function showUser(surname, name) {
          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 	}
 
 
-showUser.call(user, 'Borisov', 'Nikita');
+showUser.call(age, 'Borisov', 'Nikita');
